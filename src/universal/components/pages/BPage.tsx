741cfc5100866076f52d1f548b5a00210e06de85
holyshared <holy.shared.design@gmail.com>
2020-02-15 22:51:30 +0900

    add Helmet component

--- src/universal/components/pages/BPage.tsx
+++ src/universal/components/pages/BPage.tsx
@@ -1,7 +1,15 @@
 import React from 'react';
+import { Helmet } from "react-helmet";
 
 export const BPage = () => {
   return (
-    <h1>B</h1>
+    <React.Fragment>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>My Title</title>
+        <link rel="canonical" href="http://mysite.com/example" />
+      </Helmet>
+      <h1>B</h1>
+    </React.Fragment>
   );
 };
--- src/universal/components/pages/TopPage.tsx
+++ src/universal/components/pages/TopPage.tsx
@@ -1,7 +1,15 @@
 import React from 'react';
+import { Helmet } from "react-helmet";
 
 export const TopPage = () => {
   return (
-    <h1>Top</h1>
+    <React.Fragment>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>My Title</title>
+        <link rel="canonical" href="http://mysite.com/example" />
+      </Helmet>
+      <h1>Top</h1>
+    </React.Fragment>
   );
 };
