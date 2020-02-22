2e08d48f1d644b5889cb1913e17b860ca77621a3
holyshared <holy.shared.design@gmail.com>
2020-02-22 14:41:39 +0900

    add return type

--- src/universal/components/pages/TopPage.tsx
+++ src/universal/components/pages/TopPage.tsx
@@ -1,11 +1,11 @@
 import React, { useState, FunctionComponentElement } from "react";
 import { Header } from "../Header";
 
-const Opened = () => (<p>opened</p>);
+const Opened = (): FunctionComponentElement<{}> => <p>opened</p>;
 
 export const TopPage = (): FunctionComponentElement<{}> => {
   const [opened, open] = useState(false);
-  const click = () => open(!opened);
+  const click = (): void => open(!opened);
   return (
     <React.Fragment>
       <Header>
@@ -13,7 +13,11 @@ export const TopPage = (): FunctionComponentElement<{}> => {
         <meta name="keywords" content="blog" />
       </Header>
       <h1>Top</h1>
-      <p><button type="button" onClick={click}>Click</button></p>
+      <p>
+        <button type="button" onClick={click}>
+          Click
+        </button>
+      </p>
       {opened ? <Opened /> : null}
     </React.Fragment>
   );
