4844323a1835c8b902365ea3da11abf5e53268f5
holyshared <holy.shared.design@gmail.com>
2020-02-23 13:41:23 +0900

    fix host name

--- src/universal/graphql/client.ts
+++ src/universal/graphql/client.ts
@@ -2,13 +2,19 @@ import ApolloClient from "apollo-boost";
 import fetch from "isomorphic-fetch";
 
 const DEVELOPMENT_HOST = "localhost:5000";
+const PRODUCTION_HOST = process.env.HOST_NAME;
+
+const isBrowser = typeof window === "object";
+const isProduction = process.env.NODE_ENV === 'production';
 
 export const GRAPHQL_ENDPOINT = (function(): string {
   let protocol = "http:";
   let host = DEVELOPMENT_HOST;
-  if (typeof window === "object" && window.location.host !== DEVELOPMENT_HOST) {
+  if (isBrowser && window.location.host !== DEVELOPMENT_HOST) {
     host = window.location.host;
     protocol = window.location.protocol;
+  } else if (!isBrowser && isProduction) {
+    host = PRODUCTION_HOST;
   }
   return `${protocol}//${host}`;
 })();
